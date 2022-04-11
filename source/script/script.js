eebaa7263f8ee8c86815d9fe6c21022c9d7fc5d6
Your Name <you@example.com>
2022-04-11 14:27:30 +0300

    add showing all notes from Local storage

--- source/script/script.js
+++ source/script/script.js
@@ -14,8 +14,37 @@ addBox.addEventListener("click", () =>{
 });
 
 closeIcon.addEventListener("click", () =>{
+    titleTag.value = "";
+    descTag.value = "";
     popupBox.classList.remove("show")
 });
+
+function showNotes(){
+    document.querySelectorAll(".note").forEach(note => note.remove());
+    notes.forEach((note) =>{
+        let liTag = `<li class="note">
+        <div class="details">
+            <p>${note.title}</p>
+            <span>
+                ${note.description}
+            </span>
+            <div class="bottom-content">
+                <span>${note.date}</span>
+                <div class="settings">
+                    <i class="uil uil-ellipsis-h"></i>
+                    <ul class="menu">
+                        <li><i class="uil uil-pen"></i>Edit</li>
+                        <li><i class="uil uil-trash"></i>Delete</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </li>`;
+    addBox.insertAdjacentHTML("afterend", liTag);
+    });
+}
+
+showNotes();
 
 addBtn.addEventListener("click", e =>{
     e.preventDefault();
@@ -35,5 +64,6 @@ addBtn.addEventListener("click", e =>{
         notes.push(noteInfo);
         localStorage.setItem("notes", JSON.stringify(notes));
         closeIcon.click();
+        showNotes();
     }
 });
