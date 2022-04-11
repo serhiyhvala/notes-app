01f0d37bf33555ea0007a98288a99861ba60ebde
Your Name <you@example.com>
2022-04-11 14:38:59 +0300

    create showing settings menu

--- source/script/script.js
+++ source/script/script.js
@@ -21,7 +21,7 @@ closeIcon.addEventListener("click", () =>{
 
 function showNotes(){
     document.querySelectorAll(".note").forEach(note => note.remove());
-    notes.forEach((note) =>{
+    notes.forEach((note, index) =>{
         let liTag = `<li class="note">
         <div class="details">
             <p>${note.title}</p>
@@ -31,10 +31,10 @@ function showNotes(){
             <div class="bottom-content">
                 <span>${note.date}</span>
                 <div class="settings">
-                    <i class="uil uil-ellipsis-h"></i>
+                    <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
                     <ul class="menu">
                         <li><i class="uil uil-pen"></i>Edit</li>
-                        <li><i class="uil uil-trash"></i>Delete</li>
+                        <li onclick="deleteNote(${index})"><i class="uil uil-trash"></i>Delete</li>
                     </ul>
                 </div>
             </div>
@@ -45,6 +45,15 @@ function showNotes(){
 }
 
 showNotes();
+
+function showMenu(elem) {
+    elem.parentElement.classList.add("show");
+    document.addEventListener("click", e =>{
+        if(e.target.tagName != "I" || e.target != elem){
+            elem.parentElement.classList.remove("show");
+        }
+    });
+}
 
 addBtn.addEventListener("click", e =>{
     e.preventDefault();
